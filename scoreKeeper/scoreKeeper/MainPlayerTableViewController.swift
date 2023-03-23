8a1c6df44b63a6ff0b101aca147f1f76192e12a0
kole ervine <koleervine@gmail.com>
2023-03-23 08:02:02 -0600

    added sort for players

--- scoreKeeper/scoreKeeper/MainPlayerTableViewController.swift
+++ scoreKeeper/scoreKeeper/MainPlayerTableViewController.swift
@@ -1,20 +1,23 @@
 import UIKit
 
+// add ability to add players
+
 class MainPlayerTableViewController: UITableViewController {
     
-    //show players in order by score
-    // add ability to add players
-    
-    var basePlayers = [Player(name: "player One", score: 100), Player(name: "Kole", score: 5), Player(name: "Someone", score: 500)]
+    // array of players
+    var basePlayers = [Player(name: "player One", score: 10), Player(name: "Kole", score: 5), Player(name: "Someone", score: 50)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // sorted array of players below ** likely will need to change here for adding/added players
+        basePlayers.sort(by: { $0.score > $1.score })
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return basePlayers.count
     }
-
+    
+    // create cells for players to inhabit
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "playerCell", for: indexPath)
         
